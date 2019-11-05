c27ae6d89da583d635a54ad2eba6a2a45e258b6d
drom98 <diogoomarques98@gmail.com>
2019-11-05 16:23:36 +0000

    Add logout

--- php/login_registo/registo.php
+++ php/login_registo/registo.php
@@ -11,18 +11,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = md5(mysqli_real_escape_string($conn, $_POST['password']));
     
     if(queryNomeUser($conn, $nome)) {
-      if(queryUserPassword($conn, $nome, $password)) {
-        $row = queryUserPassword($conn, $nome, $password);
-        defineSessionVariables($row);
-        verificarTipoUtilizador($row);
-      } else {
-        //Password errada
-        header("Location: ../../pages/login.php?erro=password");
-      }
+      
     } else {
       //Username não existe
-      header("Location: ../../pages/login.php?erro=nome");
+      header("Location: ../../pages/registo.php?erro=nome");
     }
-  }
+}
 
 ?>

5038579516dde9f4a1af4e7054155b1b0e00dde0
drom98 <diogoomarques98@gmail.com>
2019-11-13 20:39:27 +0000

    Icones na navbar

--- pages/parts/navbar.php
+++ pages/parts/navbar.php
@@ -12,7 +12,7 @@ if(file_exists('../backend/utils.php')) {
 <nav class="navbar is-link is-fixed-top" role="navigation">
 	<div class="container">
 	  <div class="navbar-brand">
-	    <a href=<?php echo(PAGES.'index.php') ?> class="navbar-item">ESTClinic</a>	
+	    <a href=<?php echo(PAGES.'index.php') ?> class="navbar-item"><strong class="has-text-dark">EST</strong>Clinic</a>	
 	    <!--Adicionar class 'is-active' para mostrar X-->
 	    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
 	      <span aria-hidden="true"></span>
@@ -23,11 +23,11 @@ if(file_exists('../backend/utils.php')) {
 	  <div class="navbar-end">
 			<?php 
 			if(!verificarSessao()) { ?>
-				<a href=<?php echo(PAGES.'admin/admin.php') ?> class="navbar-item"><?php echo($_SESSION['nome']) ?><span class="icon" style="margin-left: 2px"><i class="fas fa-user"></i></span></a>
+				<a href=<?php echo(PAGES.'admin/admin.php') ?> class="navbar-item"><?php echo($_SESSION['nome']) ?><span class="icon" style="margin-left: 2px"><i class="fas fa-user-circle"></i></span></a>
 				<a href=<?php echo(BACKEND.'login_registo/logout.php') ?> class="navbar-item">Sair<span class="icon" style="margin-left: 3px"><i class="fas fa-sign-out-alt"></i></span></a> <?php
 			} else { ?>
-				<a href=<?php echo(PAGES.'login.php') ?> class="navbar-item">Login</a>
-				<a href=<?php echo(PAGES.'registo.php') ?> class="navbar-item">Registo</a> <?php
+				<a href=<?php echo(PAGES.'login.php') ?> class="navbar-item">Iniciar Sessão<span class="icon" style="margin-left: 3px"><i class="fas fa-user-circle"></i></span></a>
+				<a href=<?php echo(PAGES.'registo.php') ?> class="navbar-item">Criar Conta<span class="icon" style="margin-left: 3px"><i class="fas fa-pen-square"></i></span></a> <?php
 			}
 			?>
 	  </div>
